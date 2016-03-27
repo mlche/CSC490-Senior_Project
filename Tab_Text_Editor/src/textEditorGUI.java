837646338fc119c08b12a7ca52ba7e464cd49bbc
ken122624 <ken122624@gmail.com>
2016-03-27 16:17:38 -0400

    Worked on CLOSE operation.

--- Tab_Text_Editor/src/textEditorGUI.java
+++ Tab_Text_Editor/src/textEditorGUI.java
@@ -1,32 +1,62 @@
 
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
+import java.awt.event.*;
 
 
 class textEditorGUI{
+
+    //JFrame to hold all components
+    JFrame frm;
+
+    //Menu items
+    private JMenuItem itemOpen;
+    private JMenuItem itemPrint;
+    private JMenuItem itemClose;
+
     public textEditorGUI() {
         //  set up the JFrame for the gui
-        JFrame frm = new JFrame();
-        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frm = new JFrame();
+
+        //Do nothing on close so that windows event can give a warning/save document before closing
+        frm.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+        //Add the close listener to the frame that will listen for window closing
+        frm.addWindowListener(new CloseListener());
+
         frm.setSize(700,760);
         frm.setTitle("Tablature Text Editor");
+        frm.setLocationRelativeTo(null);  //Centers the frame on the screen
         
         //  set up a menu bar components for user to access
         //  different functionalities
         JMenuBar menubar = new JMenuBar();
         JMenu menu1 = new JMenu(" File ");
         JMenu menu2 = new JMenu(" Edit ");
+        JMenu menu3 = new JMenu("Insert");
+        JMenu menu4 = new JMenu("Help");
         menubar.add(menu1);
         menubar.add(menu2);
-        
-        //  set up submenu components
-        JMenuItem jmItem1 = new JMenuItem("Open...");
-        JMenuItem jmItem2 = new JMenuItem("Print");
-        JMenuItem jmItem3 = new JMenuItem("Close");
-        menu1.add(jmItem1);
-        menu1.add(jmItem2);
-        menu1.add(jmItem3);
-        
+        menubar.add(menu2);
+        menubar.add(menu3);
+        menubar.add(menu4);
+
+        //  set up submenu components for File menu
+        itemOpen = new JMenuItem("Open...");
+        itemPrint = new JMenuItem("Print");
+        itemClose = new JMenuItem("Close");
+        menu1.add(itemOpen);
+        menu1.add(itemPrint);
+        menu1.add(itemClose);
+
+        //Set up submenu components for Edit menu
+
+        //Set up submenu components for Insert menu
+
+        //Set up submenu components for Help Menu
+
         //  set up the gui under the menu as a text field
         JTextArea jtext = new JTextArea();
         jtext.setBorder(
@@ -43,4 +73,61 @@ class textEditorGUI{
         
     }
 
+    /**
+     * Private class for menu item listener.
+     * When menu item is clicked, corresponding action will be triggered.
+     */
+    private class MenuItemListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JMenuItem item = (JMenuItem)e.getSource();
+
+            //Determine the menu item selected
+            if(item == itemOpen){
+                //Open JFileChooser
+            }
+            else if(item == itemPrint){
+                //Do print operation
+            }
+            else if(item == itemClose){
+                //Close the program
+            }
+        }
+    }
+
+    /**
+     * Private class to listen for close operation on application.
+     * This will issue a warning to the user before exiting.
+     */
+    private class CloseListener extends WindowAdapter {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            JFrame frame = (JFrame)e.getSource();
+            boolean close;  //Will be set to true unless user selects "Cancel"
+
+            //Prompt user to save document with "Yes" "No" or "Cancel"
+            int n = JOptionPane.showConfirmDialog(frame,
+                    "Would you like to save before exiting?",
+                    "Exiting Text Editor",
+                    JOptionPane.YES_NO_CANCEL_OPTION);
+
+            //Get user's answer from prompt
+            if(n == JOptionPane.YES_OPTION){
+                //Save their document
+                close = true;
+            }
+            else if(n == JOptionPane.NO_OPTION){
+                close = true;
+            }
+            else {
+                //"Cancel" option was selected.  Do not close application.
+                close = false;
+            }
+
+            //Close application if close == true
+            if(close){
+                System.exit(0);
+            }
+        }
+    }
 }
