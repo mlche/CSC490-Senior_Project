7ebaec056dfe70da31932fde1b29d6da1b9c9245
Matt <mlcheck@uncg.edu>
2016-04-05 20:35:21 -0400

    Update 4/5

--- Tab_Text_Editor/src/TabFileChooser.java
+++ Tab_Text_Editor/src/TabFileChooser.java
@@ -4,4 +4,16 @@ import javax.swing.*;
  * JFileChooser class for file chooser window.
  */
 public class TabFileChooser extends JFileChooser{
+    public TabFileChooser(){
+        //JButton jb = new JButton();
+        JFileChooser jfc = new JFileChooser();
+        //jfc.setDialogTitle("FIle Location");
+        int returnVal = jfc.showOpenDialog(jfc);
+        //if(jfc.showOpenDialog(jb) == JFileChooser.APPROVE_OPTION){}
+        System.out.println(jfc.getSelectedFile());
+    }
+    
+    public void openFile(){
+        
+    }
 }
