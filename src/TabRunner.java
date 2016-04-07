2047adc6e6bb2adc678b8eeeec937d90b06490b7
ken122624 <ken122624@gmail.com>
2016-04-07 10:56:03 -0400

    Made alternate GUI for unlimited width.

--- src/TabRunner.java
+++ src/TabRunner.java
@@ -4,7 +4,7 @@
 public class TabRunner {
 
     public static void main(String[] args) {
-       // new UnlimitedWidthGUI();
+        //new UnlimitedWidthGUI();
         new textEditorGUI();
     }
 }
