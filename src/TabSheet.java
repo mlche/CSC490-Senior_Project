74ee6e0a72208abb94cdf1826fea2e251720c7e2
Matt <mlcheck@uncg.edu>
2016-04-16 22:44:03 -0400

    auto populate '-' in subsequent lines of tab as user inputs numbers or presses space bar (alternate way to extend the tab lines). Because the dashes will populate in each line like this, the blank tabs in the files have been shortened.

--- src/TabSheet.java
+++ src/TabSheet.java
@@ -68,10 +68,11 @@ public class TabSheet extends JScrollPane {
         textArea.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_INSERT){
+                int code = e.getKeyCode();
+                if(code == KeyEvent.VK_INSERT){
                     toggleOverwriteMode();
                 }
-                else if(e.getKeyCode() == KeyEvent.VK_MINUS){
+                else if((47 < code && code < 58) || code == KeyEvent.VK_MINUS) {
 
                     //Get current overwrite mode
                     boolean currentOverMode = overwriteMode;
@@ -80,11 +81,17 @@ public class TabSheet extends JScrollPane {
                     setOverwriteMode(false);
 
                     if(autoIncrease) {
-                        increaseTabLength();
+                        increaseTabLength(code);
                     }
 
                     setOverwriteMode(currentOverMode);
                 }
+                //else if(code == 8) decreaseTabLength();
+            }
+            public void keyReleased(KeyEvent e){
+                int code = e.getKeyCode();
+
+                if(code == KeyEvent.VK_SPACE && autoIncrease) increaseTabLength(code);
             }
         });
 
@@ -160,7 +167,7 @@ public class TabSheet extends JScrollPane {
     }
 
     /**
-     * Get auto-incrase mode
+     * Get auto-increase mode
      */
     public Boolean getAutoIncrease() {
         return autoIncrease;
@@ -172,33 +179,55 @@ public class TabSheet extends JScrollPane {
      * This will result in a set of lines that are in the same section of tablature that the cursor is in.
      * Now increase each of these lines by one '-' symbol.
      */
-    private void increaseTabLength(){
+    private void increaseTabLength(int code){
         //Create an array with enough indexes for each line in the text area.
         //String[] lines = new String[textArea.getLineCount()];
         ArrayList<String> lines = new ArrayList<>();
 
+
         try {
             //Get the line that the offset is on
             int pos = textArea.getLineOfOffset(textArea.getCaretPosition());
-            System.out.println(pos);
 
             //Fill an array with all lines in the text area
-            int i = 0;
             for (String line : textArea.getText().split("\\n")) {
                 lines.add(line);
-                i++;
             }
 
             //Check to see if line of cursor is blank, if don't increase any tabs
-            if (!lines.get(pos).equals("")) {
+            char x, y;
+            if(lines.get(pos).length()>2){
+                x = lines.get(pos).charAt(1);
+                y = lines.get(pos).charAt(2);
+            }
+            else{
+                x = ' ';
+                y = ' ';
+            }
+
+            if (x == '|' || y == '|') {
+
+                if(code == KeyEvent.VK_SPACE){
+                    int caret = textArea.getCaretPosition();
+                    String s = textArea.getText().substring(0,caret-1) + "-"
+                            + textArea.getText().substring(caret,textArea.getText().length());
+                    textArea.setText(s);
+
+                    textArea.setCaretPosition(caret);
+
+                }
+
                 //Fill an array with all lines previous to the offset line UP TO A BLANK LINE
                 //Add elements to the front of the array list since they are being read backwards
                 //ArrayList<String> prev = new ArrayList<String>();
                 ArrayList<Integer> prev = new ArrayList<Integer>();
                 for (int j = pos - 1; j >= 0; j--) {
-                    if (!lines.get(j).equals("")) {
-                        prev.add(0, j);
-                    } else
+                    if(lines.get(j).length() > 2) {
+                        if (lines.get(j).charAt(1) == '|' || lines.get(j).charAt(2) == '|') {
+                            prev.add(0, j);
+                        }
+                    }
+                    else
                         break;
                 }
 
@@ -206,9 +235,12 @@ public class TabSheet extends JScrollPane {
                 //ArrayList<String> next = new ArrayList<String>();
                 ArrayList<Integer> next = new ArrayList<Integer>();
                 for (int k = pos + 1; k < lines.size(); k++) {
-                    if (!lines.get(k).equals("")) {
-                        next.add(k);
-                    } else
+                    if(lines.get(k).length() > 2){
+                        if (lines.get(k).charAt(1) == '|' || lines.get(k).charAt(2) == '|') {
+                            next.add(k);
+                        }
+                    }
+                    else
                         break;
                 }
 
@@ -228,4 +260,96 @@ public class TabSheet extends JScrollPane {
             e1.printStackTrace();
         }
     }
+
+    /*
+    private void decreaseTabLength(){
+        //Create an array with enough indexes for each line in the text area.
+        //String[] lines = new String[textArea.getLineCount()];
+        ArrayList<String> lines = new ArrayList<>();
+
+
+        try {
+            //Get the line that the offset is on
+            int pos = textArea.getLineOfOffset(textArea.getCaretPosition());
+
+
+            //Fill an array with all lines in the text area
+            for (String line : textArea.getText().split("\\n")) {
+                lines.add(line);
+            }
+
+            //Check to see if line of cursor is blank, if don't increase any tabs
+            char x, y;
+            if(lines.get(pos).length()>2){
+                x = lines.get(pos).charAt(1);
+                y = lines.get(pos).charAt(2);
+            }
+            else{
+                x = ' ';
+                y = ' ';
+            }
+
+            if (x == '|' || y == '|') {
+
+                //Fill an array with all lines previous to the offset line UP TO A BLANK LINE
+                //Add elements to the front of the array list since they are being read backwards
+                //ArrayList<String> prev = new ArrayList<String>();
+                ArrayList<Integer> prev = new ArrayList<Integer>();
+
+                String s = "";
+                for (int j = pos - 1; j >= 0; j--) {
+                    if(lines.get(j).length() > 2) {
+                        if (lines.get(j).charAt(1) == '|' || lines.get(j).charAt(2) == '|') {
+                            prev.add(0, j);
+                        }
+                    }
+                    else
+                        break;
+                }
+
+                s = "";
+
+                //Fill an array with all lines INCLUDING and after the offset line UP TO A BLANK LINE
+                //ArrayList<String> next = new ArrayList<String>();
+                ArrayList<Integer> next = new ArrayList<Integer>();
+
+                for (int k = pos + 1; k < lines.size(); k++) {
+                    if(lines.get(k).length() > 2){
+                        if (lines.get(k).charAt(1) == '|' || lines.get(k).charAt(2) == '|') {
+                            next.add(k);
+                        }
+                    }
+                    else
+                        break;
+                }
+
+                //Combine the two lists to get the whole section of tablature
+                ArrayList<Integer> section = new ArrayList<>(prev);
+                section.addAll(next);
+
+                //Add a dash to each line in section
+
+                String alt = textArea.getText().substring(0,textArea.getLineStartOffset(section.get(0)));
+                int end = 0;
+                //for(int j = 0; j < section.size(); j++){
+                //    alt += textArea.getText().substring(textArea.getLineStartOffset(section.get(j)),
+                //                textArea.getLineEndOffset(section.get(j)) - 2) + "|\n";
+                    //if((j + 1) <= section.size()
+                    //        && (textArea.getLineEndOffset(section.get(j)) + 2) < (textArea.getLineStartOffset(section.get(j+1)))){
+                    //    alt += textArea.getText().substring(textArea.getLineEndOffset(section.get(j)),
+                    //            textArea.getLineStartOffset(section.get(j+1)));
+                    //}
+                //    end = j;
+                //}
+
+                //alt += textArea.getText().substring(textArea.getLineEndOffset(section.get(end)),
+                //        textArea.getText().length());
+                textArea.setText(alt);
+            }
+        }
+        catch(BadLocationException e1){
+            e1.printStackTrace();
+        }
+    }*/
+
 }
